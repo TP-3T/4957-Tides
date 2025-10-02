376d632bbcd19aa7115f05e850e3a8e41d3337a0
Phyo Thu Kha <phyotkha@outlook.com>
2025-10-02 16:09:11 -0700

    Revert "Merge remote-tracking branch 'origin/core' into dev"
    This reverts commit 14ced0b4477a2f06223650810f798837467cb0d2, reversing
    changes made to b078fb77d9d70069e0d5b4535d7cc578c70d8e2e.

--- Assets/Scripts/HexCell.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class HexCell : MonoBehaviour
-{
-    [SerializeField] public CubeCoordinates CellCubeCoordinates;
-    [SerializeField] public Vector3 CellPosition;
-    public MapTileData MapTileData;
-    public TerrainType TerrainType;
-}
