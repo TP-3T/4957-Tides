8ae6a89f840bfae3a4559d1f2defcfbab98b1c1f
Alex Choi <sequoia.alex.ca@gmail.com>
2025-10-06 03:32:46 -0700

    Added water level gauge. Currently implemented W or up arrow to rise, or S or down arrow to lower. But it is somewhat buggy and the text needs to be fixed.

---
+++ Assets/Scripts/WaterLevelControl.cs
@@ -0,0 +1,83 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterLevelControl : MonoBehaviour
+{
+    [SerializeField]
+    private WaterLevel _waterLevel;
+
+    [SerializeField]
+    private RectTransform _barRect;
+
+    [SerializeField]
+    private RectMask2D _mask;
+
+    [SerializeField]
+    private TMP_Text _waterLevelText;
+
+    [Tooltip("The amount to increase or decrease the water level by for testing.")]
+    private int _testChangeAmount = 1;
+
+    private float _maxBarHeight;
+    private float _initialTopMaskPadding;
+
+    void Start()
+    {
+        _maxBarHeight = _barRect.rect.height - _mask.padding.w - _mask.padding.y;
+        _waterLevelText.SetText($"{_waterLevel.WaterLevelValue}/{_waterLevel.MaxWaterLevel}");
+        _initialTopMaskPadding = _mask.padding.w;
+        SetValue(_waterLevel.WaterLevelValue);
+    }
+
+    public void SetValue(int newValue)
+    {
+        // var targetHeight = newValue * _maxTopHeight / _waterLevel.MaxWaterLevel;
+        // var newTopPadding = _maxTopHeight + _initialTopPadding - targetHeight;
+        // var padding = _mask.padding;
+        // padding.y = newTopPadding;
+        // _mask.padding = padding;
+        // _waterLevelText.SetText($"{_waterLevel.WaterLevelValue}/{_waterLevel.MaxWaterLevel}");
+
+        float ratio = (float)newValue / _waterLevel.MaxWaterLevel;
+        float targetFilledHeight = ratio * _maxBarHeight;
+        float newTopPadding = (_maxBarHeight - targetFilledHeight) + _initialTopMaskPadding;
+        newTopPadding = Mathf.Clamp(
+            newTopPadding,
+            _initialTopMaskPadding,
+            _maxBarHeight + _initialTopMaskPadding
+        );
+        var padding = _mask.padding;
+        padding.w = newTopPadding;
+        _mask.padding = padding;
+        _waterLevelText.SetText($"{newValue}/{_waterLevel.MaxWaterLevel}");
+        _waterLevelText.ForceMeshUpdate();
+    }
+
+    void Update()
+    {
+        TestInput();
+    }
+
+    private void TestInput()
+    {
+        bool changed = false;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _waterLevel.RiseWaterLevel(_testChangeAmount);
+            changed = true;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _waterLevel.LowerWaterLevel(_testChangeAmount);
+            changed = true;
+        }
+
+        if (changed)
+        {
+            SetValue(_waterLevel.WaterLevelValue);
+            Debug.Log($"Water Level changed to {_waterLevel.WaterLevelValue}.");
+        }
+    }
+}
