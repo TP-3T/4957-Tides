8c91a779386fc9e552fff92683a04424923c522a
Rodrigo <rodrigo@vancouver4us.com>
2025-10-09 14:03:04 -0700

    Added PlayerResources assembly

--- Assets/Scripts/PlayerResources/PlayerResource.cs
+++ Assets/Scripts/PlayerResources/PlayerResource.cs
@@ -1,44 +1,47 @@
 using UnityEngine;
 
-/// <summary>
-/// Represents a player resource, such as a material or population count.
-/// </summary>
-[CreateAssetMenu(fileName = "PlayerResource", menuName = "Scriptable Objects/PlayerResource")]
-public class PlayerResource : ScriptableObject
+namespace PlayerResources
 {
-    [Tooltip("Name of the resource")]
-    [SerializeField]
-    private string resourceName;
-
-    [Tooltip("How much of this resource the player has")]
-    [SerializeField]
-    private int amount;
-
     /// <summary>
-    /// The name of this resource.
+    /// Represents a player resource, such as a material or population count.
     /// </summary>
-    public string Name => resourceName;
-
-    /// <summary>
-    /// How much of this resource the player has.
-    /// </summary>
-    public int Amount => amount;
-
-    /// <summary>
-    /// Set the amount of this resource to <i>value</i>.
-    /// </summary>
-    /// <param name="value"></param>
-    public void Set(int value)
+    [CreateAssetMenu(fileName = "PlayerResource", menuName = "Scriptable Objects/PlayerResource")]
+    public class PlayerResource : ScriptableObject
     {
-        amount = value;
-    }
+        [Tooltip("Name of the resource")]
+        [SerializeField]
+        private string resourceName;
 
-    /// <summary>
-    /// Increase the amount of this resource by <i>value</i>.
-    /// </summary>
-    /// <param name="value"></param>
-    public void ApplyChange(int value)
-    {
-        amount += value;
+        [Tooltip("How much of this resource the player has")]
+        [SerializeField]
+        private int amount;
+
+        /// <summary>
+        /// The name of this resource.
+        /// </summary>
+        public string Name => resourceName;
+
+        /// <summary>
+        /// How much of this resource the player has.
+        /// </summary>
+        public int Amount => amount;
+
+        /// <summary>
+        /// Set the amount of this resource to <i>value</i>.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Set(int value)
+        {
+            amount = value;
+        }
+
+        /// <summary>
+        /// Increase the amount of this resource by <i>value</i>.
+        /// </summary>
+        /// <param name="value"></param>
+        public void ApplyChange(int value)
+        {
+            amount += value;
+        }
     }
 }
