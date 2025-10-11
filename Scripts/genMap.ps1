002ba542601a70f7366301abafd0d426df87907a
LordWilliam <willjordan05@icloud.com>
2025-10-10 23:48:30 -0700

    WO: added the real thing which was the reason for me updating the script.

--- Scripts/genMap.ps1
+++ Scripts/genMap.ps1
@@ -1,7 +1,30 @@
+$TurnTheTides = @"
+  `e[48;5;21m              `e[0m
+`e[48;5;19m     `e[0m `e[48;5;20m `e[0m`e[48;5;21m     `e[0m `e[48;5;19m     `e[0m
+ `e[48;5;17m `e[0m`e[48;5;19m  `e[0m  `e[48;5;20m `e[0m`e[48;5;21m     `e[0m  `e[48;5;17m `e[0m`e[48;5;19m  `e[0m
+ `e[48;5;17m `e[0m`e[48;5;19m  `e[0m  `e[48;5;20m `e[0m`e[48;5;21m     `e[0m  `e[48;5;17m `e[0m`e[48;5;19m  `e[0m
+"@
 $NumberTileType = @{
   0 = "plains";
   1 = "desert";
   2 = "coastal";
+}
+
+function EpicTitle {
+  Write-Host @"
+
+$TurnTheTides
+
+GenerateMap
+  -Name     # the name of the map
+  -Width    # the x plane limit
+  -Heigth   # the z plane limit
+  -Variance # How much the terrain hight will varry
+
+GenerateMap
+  -MapDetails # Array of hashmaps having the same keys
+
+"@
 }
 
 function GetRandomTile {
@@ -60,5 +83,7 @@ function GenerateMaps {
       -Depth 3 > "$PSScriptRoot\..\Assets\Maps\$($Map.Name).json"
   }
 }
+
+EpicTitle
 
 
