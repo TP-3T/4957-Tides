5ab0ead7be0347cb01670d77e203651b04bbfcaf
Alex He <donut11one@gmail.com>
2025-11-05 19:42:05 -0800

    Spawned in map
    Menu scene now has map as background

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 using TTT.GameEvents;
 public class CanvasScript : MonoBehaviour
 {
+    [SerializeField]
+    private TextAsset LevelFile;
+
+    [SerializeField]
+    private GameEvent newMapEvent;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        newMapEvent.Raise(new NewMapEventArgs() { DataFile = LevelFile });
     }
 
     // Update is called once per frame
     void Update()
     {
-        NewMapFinishedEventArgs args = eventArgs as NewMapFinishedEventArgs;
+        // NewMapFinishedEventArgs args = eventArgs as NewMapFinishedEventArgs;
     }
 }
