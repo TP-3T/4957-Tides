a00a6cd9c0abf816b7084bdb44b49a48c3a1657f
jpg157 <jasonlau5000@gmail.com>
2025-11-13 03:11:11 -0800

    Added initial implementation of climate prediction model

---
+++ Assets/Scripts/DataClasses/ClimateModel/ClimateModelDTO.cs
@@ -0,0 +1,15 @@
+namespace TTT.DataClasses.ClimateModel;
+
+public struct ClimateModelInputDTO
+{
+    public double currTemperatureCelsius;
+    public double currSeaLevelMetres;
+    public double currAtmosphericCO2ConcentrationPpm;
+    public double changeInTimeYears;
+}
+
+public struct ClimateModelOutputDTO
+{
+    public double futureTemperatureCelsius;
+    public double futureSeaLevelMetres;
+}
