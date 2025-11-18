ef703fe35e925191b349acf0ba5ba8628b7c86da
Corey Buchan <Xouwan021592@Gmail.com>
2025-11-18 14:47:27 -0800

    Add FeatureCategory; Add AddressableAsset Group for Buildings.

--- Assets/Scripts/DataClasses/TileFeatures/BuildConstraints.cs
+++ Assets/Scripts/DataClasses/TileFeatures/BuildConstraints.cs
@@ -30,6 +30,10 @@ namespace TTT.DataClasses.TileFeatures
         /// </summary>
         [field: Tooltip("Features to either require or ban.")]
         [field: SerializeField]
-        public List<Amount<FeatureType>> FeatureConstraints { get; private set; }
+        public List<Amount<FeatureType>> FeatureConstraints
+        {
+            get;
+            private set;
+        }
     }
 }
