e1f89451c3536c836585c478302c25c7e6bc490f
brysonomicon <bslindy@gmail.com>
2025-11-19 23:44:09 -0800

    stubbed out process

---
+++ Assets/Scripts/Helpers/MapDatabaseService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TTT.DataClasses.HexData;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TTT.Helpers
+{
+    /// <summary>
+    /// This service handles talking to the database to retrieve maps and their associated details.
+    /// </summary>
+    public static class MapDatabaseService
+    {
+        // api enpoints
+        private const string BASE_URL = "https://3tdb.coreybuchan.com";
+        private const string MAPS_ENDPOINT = "/maps";
+        private const string MAP_BY_ID_ENDPOINT = "/maps/mapId";
+        private const string MAP_BY_STEAMID_ENDPOINT = "/maps/steamId";
+        private const string MAP_BY_NAME_ENDPOINT = "/maps/mapName";
+
+        // build the request
+        
+        // make the request
+        
+        // gather the response
+        
+        // parse response into classes
+        
+        // transform to unity syntax
+        
+        // pass the data to other components
+
+    }
+}
