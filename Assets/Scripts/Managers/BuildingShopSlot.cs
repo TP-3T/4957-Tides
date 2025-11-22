be8307c1ab6c7a177c77c49057d9d0f73588b0b4
Corey Buchan <Xouwan021592@Gmail.com>
2025-11-22 00:13:55 -0800

    Made the in game UI scale nicer and be readable at different resolutions. Top bar still needs lots of love.

--- Assets/Scripts/Managers/BuildingShopSlot.cs
+++ Assets/Scripts/Managers/BuildingShopSlot.cs
@@ -2,14 +2,21 @@ using TMPro;
 using TTT.DataClasses.TileFeatures;
 using TTT.GameEvents;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class BuildingShopSlot : MonoBehaviour
 {
     public FeatureType feature;
 
+    [SerializeField]
     private TextMeshProUGUI textField;
 
     public GameEvent BuildModeStartingEvent;
+
+    void Start()
+    {
+        UpdateText();
+    }
 
     public void OnClick()
     {
