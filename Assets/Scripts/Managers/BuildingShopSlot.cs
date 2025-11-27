d3cb661057685532f2009843965a0cddaf81c245
Ben H <156486625+Ultratoaster@users.noreply.github.com>
2025-11-27 14:14:02 -0800

    Core shop items again (#95)
    * create material for sea
    
    * redo folder structure for water material
    
    * import industrial building models
    
    * apply new folder structure to model materials
    
    * create model for high-density residential building
    
    * Added the LowPolyTrees model into a new folder for the Natural Assets.
    
    * add models for coal and wind power
    
    * Added LowPolyRocks.fbx to the Natural Models. Oddity with the model, when in Unity, if you rotate -90degress on the x-axis, that is how I had initially envisioned the model.
    
    * commit vince's models
    
    * fix scaling issues with natural models
    
    * add tidal power building
    
    * add nuclear power building
    
    * add building for rural residential
    
    * merging???
    
    * create ui sounds
    
    * WIP, need to get content area to expand for scrolling to work properly
    
    * add main menu song
    
    * We got menu items populating
    
    * Added more map types
    
    * meta
    
    ---------
    
    Co-authored-by: GurjeetB <gurjeetbhangoo@yahoo.com>
    Co-authored-by: Tanner Parkes <tanner.lp9@gmail.com>
    Co-authored-by: Bryson Lindy <bslindy@gmail.com>

--- Assets/Scripts/Managers/BuildingShopSlot.cs
+++ Assets/Scripts/Managers/BuildingShopSlot.cs
@@ -3,13 +3,53 @@ using TTT.DataClasses.TileFeatures;
 using TTT.GameEvents;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class BuildingShopSlot : MonoBehaviour
 {
     public FeatureType feature;
 
     [SerializeField]
-    private TextMeshProUGUI textField;
+    private TextMeshProUGUI featureNameText;
+
+    [SerializeField]
+    private Image featureIcon;
+
+    [SerializeField]
+    private Image moneyRevIcon;
+
+    [SerializeField]
+    private TextMeshProUGUI moneyRevText;
+
+    [SerializeField]
+    private Image pollRevIcon;
+
+    [SerializeField]
+    private TextMeshProUGUI pollRevText;
+
+    [SerializeField]
+    private Image moneyCostIcon;
+
+    [SerializeField]
+    private TextMeshProUGUI moneyCostText;
+
+    [SerializeField]
+    private Image energyCostIcon;
+
+    [SerializeField]
+    private TextMeshProUGUI energyCostText;
+
+    [SerializeField]
+    private Image popCostIcon;
+
+    [SerializeField]
+    private TextMeshProUGUI popCostText;
+
+    [SerializeField]
+    private Image hexConstraintIcon;
+
+    [SerializeField]
+    private HorizontalLayoutGroup hexConstraintHexContainer;
 
     public GameEvent BuildModeStartingEvent;
 
@@ -25,8 +65,116 @@ public class BuildingShopSlot : MonoBehaviour
 
     public void UpdateText()
     {
-        textField = GetComponentInChildren<TextMeshProUGUI>();
+        // Null safety
+        if (feature == null)
+            return;
 
-        textField.text = feature.DisplayName;
+        // Set feature name
+        if (featureNameText != null)
+            featureNameText.text = feature.DisplayName;
+
+        // Accumulators for each resource type
+        int totalMoneyRevenue = 0;
+        int totalPollutionRevenue = 0;
+        int totalMoneyCost = 0;
+        int totalEnergyCost = 0;
+        int totalPopulationCost = 0;
+
+        // Process all resource producers
+        foreach (var producer in feature.ResourceProducers)
+        {
+            foreach (var amount in producer.ResourceAmounts)
+            {
+                int value = amount.Count;
+
+                switch (amount.Thing.Name)
+                {
+                    case "money":
+                        totalMoneyRevenue += value;
+                        break;
+                    case "pollution":
+                        totalPollutionRevenue += value;
+                        break;
+                }
+            }
+        }
+
+        // Process costs
+        foreach (var cost in feature.Cost)
+        {
+            int value = cost.Count;
+
+            switch (cost.Thing.Name)
+            {
+                case "money":
+                    totalMoneyCost += value;
+                    break;
+                case "power":
+                    totalEnergyCost += value;
+                    break;
+                case "population":
+                    totalPopulationCost += value;
+                    break;
+            }
+        }
+
+        //This should be a resource producer IMO, but I'm not changing it now
+        if (feature.PollutionEmission > 0)
+            totalPollutionRevenue += feature.PollutionEmission;
+
+        //Manage constraint icons
+        if (feature.Constraints != null && feature.Constraints.TerrainConstraints != null)
+        {
+            // Clear existing icons
+            foreach (Transform child in hexConstraintHexContainer.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Add new icons based on terrain constraints
+            foreach (var terrain in feature.Constraints.TerrainConstraints.List)
+            {
+
+            }
+        }
+
+
+        // Display accumulated values
+        SetMoneyRevDisplay(totalMoneyRevenue);
+        SetPollutionDisplay(totalPollutionRevenue);
+        SetEnergyDisplay(totalEnergyCost);
+        SetPopulationDisplay(totalPopulationCost);
+    }
+
+    private void SetMoneyRevDisplay(int revenue)
+    {
+        if (revenue > 0 && moneyRevText != null)
+        {
+            moneyRevText.text = revenue.ToString();
+        }
+    }
+
+    private void SetPollutionDisplay(int revenue)
+    {
+        if (revenue > 0 && pollRevText != null)
+        {
+            pollRevText.text = revenue.ToString();
+        }
+    }
+
+    private void SetEnergyDisplay(int cost)
+    {
+        if (cost > 0 && energyCostIcon != null && energyCostText != null)
+        {
+            energyCostText.text = cost.ToString();
+        }
+    }
+
+    private void SetPopulationDisplay(int cost)
+    {
+        if (cost > 0 && popCostIcon != null && popCostText != null)
+        {
+            popCostText.text = cost.ToString();
+        }
     }
 }
