b21e4391b66e04ec4a9100bcb7e14788f934d954
Polina Omelyantseva <156264338+hytyz@users.noreply.github.com>
2025-11-27 22:53:15 -0800

    smol update remove repeated code

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -261,9 +261,7 @@ namespace TTT.Managers
             }
         }
 
-        private void SpawnPendingFeatures()
-        {
-            // po: the idea is that
+                    // po: the idea is that
             // OnNewMap() parses json
             // then on each tile with feature != null
             //   adds (position, featureId) to pending features,
@@ -274,48 +272,6 @@ namespace TTT.Managers
             //    creates building feature args
             //    calls FeatureBuilder.OnLoadingMapFeature(args)
             //       where BuildAt() instantiates prefab
-
-            if (!_featuresLoaded)
-            {
-                Debug.LogWarning("feature types didn't load");
-            }
-
-            int spawnedCount = 0;
-
-            foreach (var (position, featureId) in _pendingFeatures)
-            {
-                if (
-                    _featureTypesByUniqueId.TryGetValue(
-                        featureId,
-                        out FeatureType featureType
-                    )
-                )
-                {
-                    var args =
-                        ScriptableObject.CreateInstance<BuildingFeatureArgs>();
-                    args.Location = position;
-                    args.FeatureType = featureType;
-                    args.OwnedByClient = false;
-                    Debug.Log(args);
-                    BuildingFeatureEvent.Raise(args);
-                    spawnedCount++;
-                }
-                else
-                {
-                    Debug.LogWarning(
-                        $"skipped unknown feature '{featureId}' at {position}"
-                    );
-                }
-            }
-
-            if (spawnedCount > 0)
-            {
-                Debug.Log($"Spawned {spawnedCount} features from map data:");
-            }
-
-            _pendingFeatures.Clear();
-        }
-
         private IEnumerator SpawnPendingFeaturesAsync()
         {
             if (!_featuresLoaded)
