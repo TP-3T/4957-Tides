73b05959fcb33a944ee6e7bee0c003a0c2d011b0
AlfredoLuzardo <luzardoneto2005@gmail.com>
2025-11-27 17:07:12 -0800

    Phyo + Alfredo added the local map browser

--- Assets/Scripts/UI/MapBrowser.cs
+++ Assets/Scripts/UI/MapBrowser.cs
@@ -1,49 +1,98 @@
 using System.Collections.Generic;
+using System.IO;
+using Codice.Client.BaseCommands;
+using Newtonsoft.Json;
 using TMPro;
+using TTT.DataClasses.MapData;
+using Unity.VisualScripting.YamlDotNet.Serialization;
 using UnityEngine;
 using static TTT.Helpers.MapDatabaseService;
 
-public class MapBrowser : MonoBehaviour
+namespace TTT.UI
 {
-    [SerializeField]
-    public GameObject MapListItem;
-
-    [SerializeField]
-    public GameObject Content;
-
-    void Start() { }
-
-    public void FetchMapList()
+    public class MapBrowser : MonoBehaviour
     {
-        // Fetch all maps store into list, iterate through the list and create a corresponding MapListItem and append to C
-        // and add it to the MapsDisplay
-        StartCoroutine(
-            GetMapList(
-                onSuccess: (mapList) => OnMapListFetched(mapList),
-                onError: (error) => OnMapListError(error)
-            )
-        );
-    }
+        [SerializeField]
+        public GameObject MapListItem;
 
-    private void OnMapListFetched(List<MapInfo> mapInfoList)
-    {
-        Debug.Log(mapInfoList.Count);
+        [SerializeField]
+        public GameObject DBContent;
 
-        foreach (var mapInfo in mapInfoList)
+        [SerializeField]
+        public GameObject LocalContent;
+
+        void Start() { }
+
+        public void FetchMapList()
         {
-            GameObject mapItem = Instantiate(MapListItem, Content.transform);
-
-            mapItem.GetComponentInChildren<TMP_Text>().text = mapInfo.MapName;
-
-            Debug.Log("Map Info Created.0.001f");
+            // Fetch all maps store into list, iterate through the list and create a corresponding MapListItem and append to C
+            // and add it to the MapsDisplay
+            StartCoroutine(
+                GetMapList(
+                    onSuccess: (mapList) =>
+                        OnMapListFetched(mapList, DBContent),
+                    onError: (error) => OnMapListError(error)
+                )
+            );
         }
-    }
 
-    private void OnMapListError(string errorMessage)
-    {
-        Debug.LogError($"Failed to fetch map list: {errorMessage}");
-    }
+        public void FetchLocalMapList()
+        {
+            string folderPath = "./Assets/Maps";
+            string[] jsonFileNames = Directory.GetFiles(folderPath, "*.json");
 
-    // Update is called once per frame
-    void Update() { }
+            List<MapInfo> localMapInfoList = new();
+            MapData localMapData = new();
+
+            foreach (var jsonFileName in jsonFileNames)
+            {
+                string jsonString = File.ReadAllText(jsonFileName);
+
+                Debug.Log(jsonFileName);
+
+                localMapData = JsonConvert.DeserializeObject<MapData>(
+                    jsonString
+                );
+
+                localMapInfoList.Add(
+                    new MapInfo(
+                        localMapData.MapID,
+                        localMapData.MapName,
+                        localMapData.SteamID
+                    )
+                );
+            }
+
+            OnMapListFetched(localMapInfoList, LocalContent);
+        }
+
+        private void OnMapListFetched(
+            List<MapInfo> mapInfoList,
+            GameObject content
+        )
+        {
+            Debug.Log(mapInfoList.Count);
+
+            foreach (var mapInfo in mapInfoList)
+            {
+                GameObject mapItem = Instantiate(
+                    MapListItem,
+                    content.transform
+                );
+
+                mapItem.GetComponentInChildren<TMP_Text>().text =
+                    mapInfo.MapName;
+
+                Debug.Log("Map Info Created.0.001f");
+            }
+        }
+
+        private void OnMapListError(string errorMessage)
+        {
+            Debug.LogError($"Failed to fetch map list: {errorMessage}");
+        }
+
+        // Update is called once per frame
+        void Update() { }
+    }
 }
