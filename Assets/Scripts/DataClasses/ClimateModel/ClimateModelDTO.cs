66ccae2711b83f63f693ef94a033121c9ad45133
jpg157 <jasonlau5000@gmail.com>
2025-11-28 20:39:33 -0800

    remove climate data class

--- Assets/Scripts/DataClasses/ClimateModel/ClimateModelDTO.cs
+++
@@ -1,8 +0,0 @@
-// namespace TTT.DataClasses.ClimateModel
-// {
-//     public struct ClimateModelOutputDTO
-//     {
-//         public float futureTemperatureCelsius;
-//         public float futureSeaLevelMetres;
-//     }
-// }
