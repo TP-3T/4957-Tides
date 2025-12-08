8dda1db05f106a66144a19ab1a75b4393518f3f9
Corey Buchan <Xouwan021592@Gmail.com>
2025-12-07 22:23:11 -0800

    Build game in main

--- Assets/Scripts/Helpers/LoadExternalJson.cs
+++ Assets/Scripts/Helpers/LoadExternalJson.cs
@@ -1,5 +1,4 @@
 using System.IO;
-// using SFB;
 using UnityEngine;
 using WebSocketSharp;
 
@@ -7,11 +6,6 @@ namespace TTT.Helpers
 {
     public static class LoadExternalJson
     {
-        // private static readonly SFB.ExtensionFilter[] extensions = new[]
-        // {
-        //     new SFB.ExtensionFilter("Data File", new string[] { "json" }),
-        // };
-
         public static bool TryGetMapJson(
             string filePath,
             out TextAsset textAsset
@@ -27,30 +21,5 @@ namespace TTT.Helpers
             textAsset = new TextAsset(sr.ReadToEnd());
             return true;
         }
-
-        // public static bool TryGetDataJson(out TextAsset textAsset)
-        // {
-        //     textAsset = new TextAsset();
-        //     string[] paths = SelectFile();
-        //     if (paths.Length == 0)
-        //     {
-        //         return false;
-        //     }
-
-        //     using StreamReader sr = new(paths[0]);
-        //     textAsset = new TextAsset(sr.ReadToEnd());
-        //     return true;
-        // }
-
-        // private static string[] SelectFile()
-        // {
-        //     var paths = StandaloneFileBrowser.OpenFilePanel(
-        //         "Load Data",
-        //         "",
-        //         extensions,
-        //         false
-        //     );
-        //     return paths;
-        // }
     }
 }
